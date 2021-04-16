cd7c6008f7c2065284df16b3f9c65ba545210a89
daniellaga84@gmail.com <Erkano84!!>
2021-04-16 12:35:35 +0200

    Updated with new build.gradle.kts and added one } in AppApplication.kt.

--- src/main/kotlin/com/example/app/AppApplication.kt
+++ src/main/kotlin/com/example/app/AppApplication.kt
@@ -8,28 +8,32 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class AppApplication
+class AppApplication {
 
-@Bean
-fun run(repository: CoffeeShopRepository) = ApplicationRunner {
-    repository.save(CoffeeShopModel(
-        name = "Some coffeeshop 1",
-        address = "udawany adress1, Gdynia 81-341 Poland",
-        phone = "58 621 33 33",
-        priceOfCoffee = 1.50,
-        powerAccessible = true,
-        internetReliability = 5
-    ))
-    repository.save(CoffeeShopModel(
-        name = "Some coffeeshop 2",
-        address = "udawany adress 551, Gdynia 81-341 Poland",
-        phone = "58 621 33 39",
-        priceOfCoffee = 2.50,
-        powerAccessible = true,
-        internetReliability = 3
-    ))
+    @Bean
+    fun run(repository: CoffeeShopRepository) = ApplicationRunner {
+        repository.save(
+            CoffeeShopModel(
+                name = "Some coffeeshop 1",
+                address = "udawany adress1, Gdynia 81-341 Poland",
+                phone = "58 621 33 33",
+                priceOfCoffee = 1.50,
+                powerAccessible = true,
+                internetReliability = 5
+            )
+        )
+        repository.save(
+            CoffeeShopModel(
+                name = "Some coffeeshop 2",
+                address = "udawany adress 551, Gdynia 81-341 Poland",
+                phone = "58 621 33 39",
+                priceOfCoffee = 2.50,
+                powerAccessible = true,
+                internetReliability = 3
+            )
+        )
+    }
 }
-
 fun main(args: Array<String>) {
     runApplication<AppApplication>(*args)
 }
