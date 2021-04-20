136525e39ba022b85d3cf8d4f825edeac660903d
daniellaga84@gmail.com <Erkano84!!>
2021-04-20 11:27:18 +0200

    Updating CoffeeShopsList.js.

--- client/src/CoffeeShopsList.js
+++ client/src/CoffeeShopsList.js
@@ -35,4 +35,52 @@ class CoffeeShopsList extends Component {
         };
         this.remove = this.remove.bind(this);
     }
+
+    async componentDidMount() {
+        this.setState({isLoading: true});
+        const response = await this.props.api.getAll();
+        if (!response.ok){
+            this.setState({
+                errorMessage: `Faile to load coffee shops: ${response.status} ${response.statusText}`,
+                isLoading: false
+            })
+        }else {
+            const body = await response.json();
+            const coffeeShops = body._embedded.coffeeshops;
+            this.setState({
+                coffeeShops: coffeeShops,
+                isLoading: false,
+                errorMessage: null
+            });
+        }
+    }
+
+    async remove(id) {
+        let response = await this.props.api.delete(id);
+        if (!response.ok) {
+            this.setState({errorMessage: `Failed to delete coffee shop: ${response.status} ${response.statusText}`})
+        }
+        else {
+            let updatedCoffeeShops = [...this.state.coffeeShops].filter(i => i.id !== id);
+            this.setState({coffeeShops: updatedCoffeeShops, errorMessage: null});
+        }
+    }
+    render() {
+    const {coffeeShops, isLoading, errorMessage} = this.state;
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+    return (
+        <div>
+            {this.props.navbar}
+            <div className="d-flex flex-row justify-content-between p-3">
+                <h3 className="coffee-shops-title">Coffee Shops</h3>
+                <Button color="success" tag={Link} to="/coffee-shops/new">Add New</Button>
+            </div>
+            {errorMessage ?
+            }
+        </div>
+    )
+    }
 }
