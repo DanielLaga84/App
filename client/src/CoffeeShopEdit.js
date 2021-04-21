caffb1b8dcdf373ca299fa9aa79081ae818034d2
daniellaga84@gmail.com <Erkano84!!>
2021-04-21 11:14:17 +0200

    Update CoffeeShopEdit.js.

--- client/src/CoffeeShopEdit.js
+++ client/src/CoffeeShopEdit.js
@@ -25,7 +25,33 @@ class CoffeeShopEdit extends Component {
     }
 
     async componentDidMount() {
-
+    this.state.isCreate = this.props.match.params.id === `new`;
+    if (!this.state.isCreate) {
+        const response = await this.props.api.getById(this.props.match.params.id);
+        const coffeeShop = await response.json();
+        this.setState({item: coffeeShop});
+    }
+    }
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.item};
+        item[name] = value;
+        this.setState({item});
     }
 
+    async handleSubmit(event) {
+        event.preventDefault();
+        const {item, isCreate } = this.state;
+
+        let result = isCreate ? await this.props.api.create(item) : await this.props.api.update(item);
+
+        if (!result.ok) {
+            this.setState({errorMessage: `Failed to ${isCreate ? 'create' : 'update'} record: ${result.status} ${result.statusText}`})
+        } else {
+            this.setState({errorMessage: null});
+            this.props.history.push('/coffee-shops');
+        }
+    }
 }
